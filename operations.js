0ef2096e4f34a6506286540aa7be99ac75ecedbf
Ananya-1392 <ananyashankhdhar21@gmail.com>
2024-03-28 14:48:01 +0530

    js

--- operations.js
+++ operations.js
@@ -40,11 +40,11 @@ let obj = {
     address: 'Bangaluru'
 }
 console.log(Object.keys(obj)); // [name, job, salary, address]
-let ob1 = {
+let obj1 = {
     name: 'Mr X',
     job: 'Hacker',
     salary: 1000,
     address: 'Bangaluru'
 }
 let isEq1 = obj == obj1; // false
-let isEq2 = obj === obj2; // false
+let isEq2 = obj === obj1; // false
